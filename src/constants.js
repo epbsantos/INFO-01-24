a8540519e4230c97d92c4906034b729d05c22332
william-wv <140337500+william-wv@users.noreply.github.com>
2024-05-27 20:15:19 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,22 @@
 export const scaleFactor = 2;
 
 export const dialogueData = {
-  placa1: 'Bem vindo ao inforgŕafico da turma de <b>front2</b> do curso de <a href="https://tsi.td.utfpr.edu.br/" target="_blank"><b>TSI</b></a>, UTFPR Toledo PR. Ande a vontade',
-  placa2: 'Outro texto! <br> <img src="https://www.conexaoambiental.pr.gov.br/sites/conexao-ambiental/arquivos_restritos/files/styles/escala_940_largura_/public/imagem/2022-12/capa_programa_estadual_0.webp?itok=LusKjicW">',
-  txt1:   'Colocar os textos sobre educação ambiental.',
+  placa1: 'Bem vindo ao inforgŕafico da turma de front2 do curso de TSI, UTFPR Toledo PR. Ande a vontade',
 
+  placa2: 'Este infografico trata-se sobre a como podemos possuir uma concientização ambiental melhor',
 
+  placa3: 'O crescimento das emissões de gases de efeito estufa contribui para as mudanças climáticas e agrava o problema do aquecimento global. A má qualidade do ar representa riscos graves para a saúde humana.',
+
+  placa4: 'Ter atitudes sustentáveis significa estar atento às necessidades do meio ambiente e fazer escolhas que não sejam prejudiciais ao planeta. Como por exemplo isso pode significar a compra de uma garrafa de água de vidro ou cerâmica em vez de garrafas de plástico descartáveis.',
+ 
+  placa5: '',
+
+  placa6: '',
+
+  placa7: '',
+
+  placa8: '',
+  
+  
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
 };

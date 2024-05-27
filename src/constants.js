41ba4db09e0280fd1d151bc006096d860d9d1d76
RegisPauvels <152766319+RegisPauvels@users.noreply.github.com>
2024-05-27 20:59:14 -0300

    Update constants.js
    Resumo linhas de ação do projeto educação ambiental

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,17 @@ export const dialogueData = {
   placa3: 'O crescimento das emissões de gases de efeito estufa contribui para as mudanças climáticas e agrava o problema do aquecimento global. A má qualidade do ar representa riscos graves para a saúde humana.',
 
   placa4: 'Ter atitudes sustentáveis significa estar atento às necessidades do meio ambiente e fazer escolhas que não sejam prejudiciais ao planeta. Como por exemplo isso pode significar a compra de uma garrafa de água de vidro ou cerâmica em vez de garrafas de plástico descartáveis.',
- 
+  
+  linhaDeAcao1: '<b>Linha de ação 1 - Monitoramento e Avaliação de Políticas, Programas e Projetos de Educação Ambiental </b> <b>Missão:</b> Organizar e fortalecer ações efetivas de educação ambiental, visando a sustentabilidade socioambiental. <b>Produtos:</b> Ações de educação ambiental planejadas, articulações técnicas e políticas, instâncias de participação social fortalecidas, políticas monitoradas e avaliadas, diagnóstico da condição socioambiental, políticas ambientais definidas, municípios articulados, mecanismos de participação social e CIEA fortalecida.',
+  
+  linhaDeAcao2: '<b>Linha de Ação 2: Educação Ambiental Não Formal no Estado do Paraná</b> <b>Missão: Propiciar à sociedade paranaense condições para atuar de modo engajado na consolidação da gestão ambiental e territorial do Estado.</b> <b>Produtos:</b> Redes de educação ambiental, programas integrados de gestão ambiental, boas práticas e tecnologias sustentáveis divulgadas, formação de educadores ambientais, resultados e projetos com ações de educação ambiental, divulgação de protocolos internacionais e políticas ambientais, processos formativos do SISNAMA estimulados. ',
+  
+  linhaDeAcao3: '<b>Linha de Ação 3: Educação Ambiental por meio do Ensino Formal no Estado do Paraná</b> <b>Missão:</b> Implementar a educação ambiental para que as instituições de educação básica e superior do Estado do Paraná se tornem Espaços Educadores Sustentáveis, formando cidadãos comprometidos com a sustentabilidade socioambiental local e global. <b>Produtos</b> Formação de gestores e comunidade escolar, produção de conhecimento e pesquisas, novas tecnologias e materiais didáticos, PPCs com temáticas ambientais, espaços educadores sustentáveis planejados, superação da disciplinaridade, comitês de educação ambiental nas instituições.',
+  
+  linhaDeAcao4: '<b>Linha de Ação 4: Comunicação para Educação Ambiental</b> <b>Missão:</b> Promover diálogos entre diversos setores da sociedade divulgando a educação ambiental para construção de cidades e comunidades sustentáveis e socioambientalmente justas. <b>Produtos:</b>  Educomunicação para escola e comunidade, multimídias implementadas, portal de informações, materiais didáticos, EaD, programa permanente de educação ambiental à distância. ',
+  
+  linhaDeAcao5: '<b>Linha de Ação 5: Fomento e Financiamento para a Educação Ambiental</b> <b>Missão: </b>   Identificar fontes de recursos para fomento e financiamento das ações previstas no Programa Estadual de Educação Ambiental. <b>Produtos:</b> Recursos específicos para educação ambiental alocados, recursos de medidas compensatórias e multas ambientais revertidos, incentivos fiscais para empresas, recursos de todas as áreas do estado integrados, recursos financeiros de programas estaduais e de extensão rural aplicados em educação ambiental.',
+
   placa5: '',
 
   placa6: '',

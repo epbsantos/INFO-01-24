67f978c9cff093c8c215ad71acdad0eaf5b269cd
Giovane Lazzeri <150306298+giuo-lazzeri@users.noreply.github.com>
2024-06-02 00:10:22 -0300

    falas
    adicionado fala dos NPCs e Placas

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,15 @@
 export const scaleFactor = 2;
 
 export const dialogueData = {
-  placa1: 'Bem vindo ao inforgŕafico da turma de front2 do curso de TSI, UTFPR Toledo PR. Ande a vontade',
 
-  placa2: 'Este infografico trata-se sobre a como podemos possuir uma concientização ambiental melhor',
+  pl1: 'Bem vindo ao inforgŕafico da turma de front2 do curso de TSI, UTFPR Toledo PR. Ande a vontade',
 
-  placa3: 'O crescimento das emissões de gases de efeito estufa contribui para as mudanças climáticas e agrava o problema do aquecimento global. A má qualidade do ar representa riscos graves para a saúde humana.',
+  pl2: 'Este infografico trata-se sobre a como podemos possuir uma concientização ambiental melhor. ',
 
-  placa4: 'Ter atitudes sustentáveis significa estar atento às necessidades do meio ambiente e fazer escolhas que não sejam prejudiciais ao planeta. Como por exemplo isso pode significar a compra de uma garrafa de água de vidro ou cerâmica em vez de garrafas de plástico descartáveis.',
-  
+  pl3: 'O crescimento das emissões de gases de efeito estufa contribui para as mudanças climáticas e agrava o problema do aquecimento global. A má qualidade do ar representa riscos graves para a saúde humana.', 
+
+  pl4: 'Ter atitudes sustentáveis significa estar atento às necessidades do meio ambiente e fazer escolhas que não sejam prejudiciais ao planeta. Como por exemplo isso pode significar a compra de uma garrafa de água de vidro ou cerâmica em vez de garrafas de plástico descartáveis. ',
+
   linhaDeAcao1: '<b>Linha de ação 1 - Monitoramento e Avaliação de Políticas, Programas e Projetos de Educação Ambiental </b> <b>Missão:</b> Organizar e fortalecer ações efetivas de educação ambiental, visando a sustentabilidade socioambiental. <b>Produtos:</b> Ações de educação ambiental planejadas, articulações técnicas e políticas, instâncias de participação social fortalecidas, políticas monitoradas e avaliadas, diagnóstico da condição socioambiental, políticas ambientais definidas, municípios articulados, mecanismos de participação social e CIEA fortalecida.',
   
   linhaDeAcao2: '<b>Linha de Ação 2: Educação Ambiental Não Formal no Estado do Paraná</b> <b>Missão: Propiciar à sociedade paranaense condições para atuar de modo engajado na consolidação da gestão ambiental e territorial do Estado.</b> <b>Produtos:</b> Redes de educação ambiental, programas integrados de gestão ambiental, boas práticas e tecnologias sustentáveis divulgadas, formação de educadores ambientais, resultados e projetos com ações de educação ambiental, divulgação de protocolos internacionais e políticas ambientais, processos formativos do SISNAMA estimulados. ',
@@ -19,14 +20,54 @@ export const dialogueData = {
   
   linhaDeAcao5: '<b>Linha de Ação 5: Fomento e Financiamento para a Educação Ambiental</b> <b>Missão: </b>   Identificar fontes de recursos para fomento e financiamento das ações previstas no Programa Estadual de Educação Ambiental. <b>Produtos:</b> Recursos específicos para educação ambiental alocados, recursos de medidas compensatórias e multas ambientais revertidos, incentivos fiscais para empresas, recursos de todas as áreas do estado integrados, recursos financeiros de programas estaduais e de extensão rural aplicados em educação ambiental.',
 
-  placa5: '',
+  p1: 'Hello World! ',
 
-  placa6: '',
+  p2: 'Olá Mundo! ',
 
-  placa7: '',
+  p3: '안녕하세요 세계! ',
 
-  placa8: '',
-  
-  
+  p4: 'Hallo Welt! ',
+
+  p5: 'Bonjour le monde! ',
+
+  p6: 'Привет, мир! ',
+
+  p7: '你好世界! ',
+
+  p8: 'Bem vindo ao Parque da Pig City da turma de front2. ',
+
+  p9: 'Estou atrasado para o trabalho!',
+
+  p10: 'Que dia lindo!',
+
+  p11: 'Olá, precisa de ajuda? temos tomates e alface fresquinhos!',
+
+  p12: 'Olá, venha esperimentar alguns dos nossos morangos, fresquinhos!',
+
+  p13: 'Hoje é um dia ótimo para compra na feirinha da Pig City.',
+
+  p14: 'Olha a pamonha!',
+
+  p15: 'Estamos fazendo uma obra, por favor tenha cuidado, aconselho não passar!',
+
+  p16: 'Bora pessoal, voltem ao trabalho!',
+
+  p17: 'Estou cansado!',
+
+  p18: 'OK chefe!',
+
+  p19: 'Aceita um café? ',
+
+  p20: 'Temos a melhor tapioca da região!',
+
+  p21: 'Estamos com o estoque quase vazio.',
+
+  p22: 'Quero um café!',
+
+  p23: 'Loja fechada, quero ir embora!',
+
+  p24: 'Uma ótima tarde para um chimarrão!',
+
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+
 };
